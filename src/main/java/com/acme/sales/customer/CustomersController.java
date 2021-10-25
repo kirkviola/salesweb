ea85824ea3bffc868feb31a9ddd9662de19ccaa9
Matthew Kirkendall <kirk.viola@gmail.com>
2021-10-25 16:33:49 -0400

    10/25 commits

--- src/main/java/com/acme/sales/customer/CustomersController.java
+++ src/main/java/com/acme/sales/customer/CustomersController.java
@@ -1,8 +1,15 @@
 package com.acme.sales.customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +22,59 @@ public class CustomersController {
 	private CustomerRepository custRepo;
 	
 	@GetMapping
-	public Iterable<Customer> GetAll(){
-		return custRepo.findAll();
+	public ResponseEntity<Iterable<Customer>> GetAll(){
+		var customers = custRepo.findAll();
+		return new ResponseEntity<Iterable<Customer>>(customers, HttpStatus.OK); // matches in the method call
+	}
+	
+	// The get by id method
+	@GetMapping("{id}")
+	public ResponseEntity<Customer> GetById(@PathVariable int id) { // PathVariable annotation means inc from url
+		var customer = custRepo.findById(id);
+		if(customer.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Customer>(customer.get(), HttpStatus.OK); // get method returns value if present, otherwise throws exception
+	}
+	
+	// Using method from repository
+	@GetMapping("{code}/{name}")
+	public ResponseEntity<Customer> GetByCodeAndName(@PathVariable String code, @PathVariable String name){
+		var customer = custRepo.findByCodeAndName(code, name);
+		if(customer.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Customer>(customer.get(), HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Customer> Insert(@RequestBody Customer customer) {
+		if(customer == null) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		var newCustomer = custRepo.save(customer); // save returns updated instance
+		return new ResponseEntity<Customer>(newCustomer, HttpStatus.CREATED);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@PutMapping("{id}")
+	public ResponseEntity Update(@PathVariable int id, @RequestBody Customer customer){
+		if(id != customer.getId()) // Like C#, ensuring Ids match
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		var oldCustomer = custRepo.findById(id);
+		if(oldCustomer == null)
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		custRepo.save(customer);
+		 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@DeleteMapping("{id}")
+	public ResponseEntity Delete(@PathVariable int id) {
+		var customer = custRepo.findById(id);
+		if(customer.isEmpty())
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND); // Deals with non-existent entries
+		custRepo.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 }
--- src/main/java/com/acme/sales/orderline/OrderlinesController.java
+++ src/main/java/com/acme/sales/orderline/OrderlinesController.java
@@ -1,10 +1,19 @@
 package com.acme.sales.orderline;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.acme.sales.order.OrderRepository;
 
 @CrossOrigin
 @RestController
@@ -13,10 +22,69 @@ public class OrderlinesController {
 	
 	@Autowired
 	private OrderlineRepository olineRepos;
+	@Autowired
+	private OrderRepository orderRepo;
 	
 	@GetMapping
-	public Iterable<Orderline> GetAll(){
-		return olineRepos.findAll();
+	public ResponseEntity<Iterable<Orderline>> GetAll(){
+		var olines = olineRepos.findAll();	
+		return new ResponseEntity<Iterable<Orderline>>(olines, HttpStatus.OK);
 	}
-
+	
+	@GetMapping("{id}")
+	public ResponseEntity<Orderline> GetById(@PathVariable int id){
+		var oline = olineRepos.findById(id);
+		if(oline.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Orderline>(oline.get(), HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Orderline> Insert(@RequestBody Orderline orderline) throws Exception {
+		if(orderline == null) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		var newOline = olineRepos.save(orderline);
+		RecalculateOrder(orderline.getOrder().getId());
+		return new ResponseEntity<Orderline>(newOline, HttpStatus.CREATED);
+	}
+	// add recalc order
+	@PutMapping("{id}")
+	public ResponseEntity Update(@PathVariable int id, @RequestBody Orderline orderline) {
+		if (id != orderline.getId())
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		var oldOline = olineRepos.findById(id);
+		if (oldOline == null) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		olineRepos.save(orderline);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+	
+	@DeleteMapping("{id}")
+	public ResponseEntity Delete(@PathVariable int id) {
+		var oline = olineRepos.findById(id);
+		if (oline.isEmpty())
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		olineRepos.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			
+	}
+	
+	private void RecalculateOrder(int orderId) throws Exception {
+		var optOrder = orderRepo.findById(orderId);
+		if(optOrder.isEmpty()) {
+			throw new Exception("order id is invalid");			
+		}
+		var order = optOrder.get();
+		var orderLines = olineRepos.findOrderlineByOrderId(orderId);
+		var total = 0;
+		for(var orderLine : orderLines) {
+			total += orderLine.getQuantity() * orderLine.getPrice();
+		}
+		order.setTotal(total);
+		orderRepo.save(order);
+		
+	}
 }
